1dc0aca9a205b94735024b0964a6b322f92482c7
hehmdalolkek <badeha.inna@yandex.ru>
2023-12-16 17:31:05 +0400

    init

---
+++ src/main/java/com/hehmdalolkek/spring/kidscareback/KidsCareBackApplication.java
@@ -0,0 +1,13 @@
+package com.hehmdalolkek.spring.kidscareback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidsCareBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidsCareBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hehmdalolkek/spring/kidscareback/KidsCareBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hehmdalolkek.spring.kidscareback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidsCareBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
